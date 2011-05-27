613d7293183aac36cf5adff1950a513a24887c12
Mark <Mark@6db434c8-db5f-9923-23f6-6e42ee4e826f>
2011-05-27 14:27:50 +0000

    Added the KeeCracker project as the console frontend.

---
+++ KeeCrackerLib/PasswordList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace KeeCracker
+{
+    public class PasswordList : IPasswordSource
+    {
+        //--- Fields ---
+        TextReader _passwordFileReader;
+
+        //--- Constructors ---
+
+        public PasswordList(TextReader passwordFileStream)
+        {
+            _passwordFileReader = passwordFileStream;
+        }
+
+        //--- Public Methods ---
+
+        public string NextPassword()
+        {
+            return _passwordFileReader.ReadLine();
+        }
+    }
+}
