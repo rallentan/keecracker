e9ebb9473650e078bca9a67fcd48906328de8626
Mark <Mark@6db434c8-db5f-9923-23f6-6e42ee4e826f>
2011-05-31 20:55:39 +0000

    Removed copyright information from the AssemblyInfo.cs files.

--- KeeCracker/Properties/AssemblyInfo.cs
+++ KeeCracker/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("KeeCracker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KeeCracker")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
